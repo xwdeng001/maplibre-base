cf4af96165e1f496c3dbc4791b436ab7a3f7acfd
JuliusDeng <1576554007@qq.com>
2026-05-13 12:43:55 +0800

    map

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import router from './router'
 import 'ant-design-vue/dist/reset.css'
 import './style.css'
 
+const basePath = import.meta.env.BASE_URL.replace(/\/$/, '')
+const legacyPrefix = `${basePath}/index.html/`
+const { pathname, search, hash } = window.location
+
+if (!hash && pathname.startsWith(legacyPrefix)) {
+  const restPath = pathname.slice(legacyPrefix.length)
+  const normalizedRest = restPath.startsWith('/') ? restPath : `/${restPath}`
+  window.location.replace(`${basePath}/index.html#${normalizedRest}${search}`)
+}
+
 const app = createApp(App)
 app.use(Antd)
 app.use(router)
